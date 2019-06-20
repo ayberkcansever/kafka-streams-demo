f478b65f1975015cafb5b16da2d55c9f7ee6d66f
Ayberk Cansever <ayberkcansever@Ayberk-MacBook-Pro.local>
2019-06-20 15:51:02 +0300

    WordCounter added

--- src/main/java/com/canseverayberk/kafkastreamsdemo/WordCounter.java
+++ src/main/java/com/canseverayberk/kafkastreamsdemo/WordCounter.java
@@ -5,7 +5,10 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,12 +16,12 @@ import java.util.Arrays;
 import java.util.Properties;
 
 @Component
-public class TimedWindowedWordCounter {
+public class WordCounter {
 
     @PostConstruct
     public void start() {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word_timed");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
@@ -31,16 +34,14 @@ public class TimedWindowedWordCounter {
         Serde<Long> longSerde = Serdes.Long();
 
         KStream<String, String> textLines = builder.stream("word_input", Consumed.with(stringSerde, stringSerde));
-        KTable<Windowed<String>, Long> wordCounts = textLines
+        KTable<String, Long> wordCounts = textLines
                 .flatMapValues(value -> {
-                    System.out.println("consumed for timed windowed count: " + value);
+                    System.out.println("consumed for count: " + value);
                     return Arrays.asList(value.toLowerCase().split("\\W+"));
                 })
                 .groupBy((key, value) -> value)
-                .windowedBy(TimeWindows.of(60000).advanceBy(10000))
-                .count(Materialized.as("windowed_count"));
-
-        wordCounts.toStream((wk, v) -> wk.key()).to("word_timed_output", Produced.with(stringSerde, longSerde));
+                .count();
+        wordCounts.toStream().to("word_output", Produced.with(stringSerde, longSerde));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.cleanUp();
